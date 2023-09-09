5c47ce70a72154663f51c83d50013f62b2d7e546
HackDehZack <Zackseriousemail@gmail.com>
2023-09-09 02:53:48 -0400

    did the past present or future class 4 each  block

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,22 @@ $(function () {
   });
 
 
+  // Apply the past, present, or future class to each time block
+  var currentHour = dayjs().format("H");
+  $(".time-block").each(function() {
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (timeBlockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 
 
 
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
